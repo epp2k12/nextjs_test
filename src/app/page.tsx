2882564ed83097ba5706a2a70bd24ced186a3f0f
Erwinsky Presby <epp2k12@gmail.com>
2024-02-11 19:51:13 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return <h1>Welcome Home!</h1>
+}
+
+// const Home = ()=> {    
+//  return(
+//     <div>
+//     <h1>This is an Example Page</h1>
+//     <p>Hello, world!</p>
+//     </div>
+//     );
+// }
+// export default Home;
